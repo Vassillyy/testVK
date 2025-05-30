0602649be796e605e84ee38757ece7e33e116849
Василий <roman12345fak@gmail.com>
2025-05-30 22:43:22 +0300

    установка нужных зависимостей

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import {fileURLToPath} from 'url'
+import {dirname} from 'path'
+import * as path from 'node:path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/monitoring-aircrafts/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 })
